862eeb706a8444eb37b6ba6ef0401184dc82e12e
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-06-12 00:36:14 +0600

    Get all the services form the db

--- Index.js
+++ Index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+// const res = require('express/lib/response');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,20 +11,30 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_PASS}:${process.env.DB_USER}@cluster0.4ffbska.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4ffbska.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const ServiceCollection = client.db("volunteerNetwork").collection("volServices");
-  console.log('db working');
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run() {
+    try {
+        await client.connect();
+        const ServiceCollection = client.db("volunteerNetwork").collection("volServices");
 
-app.get('/', (req, res)=>{
+        app.get('/services', async (req, res) => {
+            const services = await ServiceCollection.find({}).toArray();
+            res.send(services)
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
     res.send('Volunteer Network Server')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('listening', port);
 })
